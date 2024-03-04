bd454e988622c089fe154a5affb2b61e43b05584
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-05 01:20:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+provider "oci" {
+  tenancy_ocid         = var.tenancy_id
+  user_ocid            = var.user_id
+  fingerprint          = var.api_fingerprint
+  private_key_path     = var.api_private_key_path
+  region               = var.region
+  disable_auto_retries = false
+}
+
+
 module "vcn" {
   source  = "./modules/VCN"
 

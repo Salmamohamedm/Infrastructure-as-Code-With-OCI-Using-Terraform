13237a4cf463e98f1660dc303c3a35a267aa536a
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-05 01:22:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,12 +13,12 @@ module "vcn" {
 
   # general oci parameters
   compartment_id = var.compartment_id
-  label_prefix   = var.label_prefix
+//  label_prefix   = var.label_prefix
 
   # vcn parameters
   create_internet_gateway = var.create_internet_gateway
-  create_nat_gateway      = var.create_nat_gateway
-  create_service_gateway  = var.create_service_gateway
+ // create_nat_gateway      = var.create_nat_gateway
+ // create_service_gateway  = var.create_service_gateway
   tags                     = var.freeform_tags
   vcn_cidrs                = var.vcn_cidrs
   vcn_dns_label            = var.vcn_dns_label
@@ -36,7 +36,7 @@ module "subnet" {
   enable_ipv6    = var.enable_ipv6
   vcn_id         = oci_core_vcn.vcn.id
   ig_route_id    = var.create_internet_gateway ? oci_core_route_table.ig[0].id : null
-  nat_route_id   = var.create_nat_gateway ? oci_core_route_table.nat[0].id : null
+ // nat_route_id   = var.create_nat_gateway ? oci_core_route_table.nat[0].id : null
 
   freeform_tags = var.freeform_tags
 

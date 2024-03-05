4603652e18e5bc5c3810809bae8fde1e4d2d9567
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-05 18:02:42 +0200

    Update main.tf

--- modules/VCN/main.tf
+++ modules/VCN/main.tf
@@ -3,4 +3,8 @@ resource "oci_core_virtual_network" "vcn" {
   cidr_block     = var.vcn_cidr
   dns_label      = var.vcn_dns_label
   display_name   = var.vcn_display_name
+
+  lifecycle {
+    ignore_changes = ["dns_label"]
+  }
 }

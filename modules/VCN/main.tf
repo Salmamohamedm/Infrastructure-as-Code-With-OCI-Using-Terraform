27256759e23140ceccbae1bbe75c9c9770781b60
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-05 02:03:29 +0200

    Update main.tf

--- modules/VCN/main.tf
+++ modules/VCN/main.tf
@@ -1,11 +1,6 @@
-resource "oci_core_virtual_network" vcn" {
-
-compartment_id = var.compartment_ocid
-
-cidr_block = var.vcn_cidr
-
-dns_label = var.vcn_dns_label
-
-display_name = var.vcn_dns_label
-
+resource "oci_core_virtual_network" "vcn" {
+  compartment_id = var.compartment_ocid
+  cidr_block     = var.vcn_cidr
+  dns_label      = var.vcn_dns_label
+  display_name   = var.vcn_display_name
 }

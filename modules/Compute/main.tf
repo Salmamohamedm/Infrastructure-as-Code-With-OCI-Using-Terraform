2b8c3fdcf032b0d15f8a667bf92b58e8e7e53a6c
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-05 22:40:23 +0200

    Update main.tf

--- modules/Compute/main.tf
+++ modules/Compute/main.tf
@@ -1,1 +1,11 @@
+resource "oci_core_instance" "compute_instance" {
+  compartment_id      = var.compartment_id
+  shape               = var.shape
+  display_name        = var.display_name
+  availability_domain = "${var.availability_domain}"
+  subnet_id           = var.subnet_id
+
+  metadata {
+    ssh_authorized_keys = var.ssh_public_key
+  }
 

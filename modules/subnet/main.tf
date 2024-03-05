a2b94d84d12d6cdafc8011aabd664a6f85d3c2a6
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-05 22:09:22 +0200

    Update main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,1 +1,32 @@
+resource "oci_core_subnet" "subnet" {
+  availability_domain = var.availability_domain
+  compartment_id      = var.compartment_ocid
+  vcn_id              = oci_core_virtual_network.vcn.id
+  cidr_block          = cidrsubnet(var.vcn_cidr, 8, 1)
+  display_name        = var.dns_label
+  dns_label           = var.dns_label
+  route_table_id      = oci_core_route_table.PublicRT.id
+  security_list_ids   = [oci_core_security_list.securitylist.id]
+}
+
+resource "oci_core_security_list" "securitylist" {
+  display_name   = "SL_public"
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.vcn.id
+
+  egress_security_rules {
+    protocol    = "all"
+    destination = "0.0.0.0/0"
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/8"
+
+    tcp_options {
+      min = 88
+      max = 80
+    }
+  }
+}
 

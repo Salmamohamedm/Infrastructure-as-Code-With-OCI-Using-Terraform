e994dc1792ccfbe8fd7bbcbb30d7fd9f03d57cc8
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-06 19:31:48 +0200

    Update main.tf

--- modules/IGW/main.tf
+++ modules/IGW/main.tf
@@ -3,6 +3,6 @@
 resource "oci_core_internet_gateway" "igw" {
   compartment_id = var.compartment
   display_name   = "${var.vcn_dns_label}-igw"
-  vcn_id         = var.vcn.id
+  vcn_id         = var.vcn_id
 }
 

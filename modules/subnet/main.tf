5ad5f4e9be4a6653cb48b866549f2bf91973295a
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-06 17:47:24 +0200

    Update main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -4,7 +4,7 @@ resource "oci_core_subnet" "subnet" {
   vcn_id              = oci_core_virtual_network.vcn.id
   cidr_block          = cidrsubnet(var.vcn_cidr, 8, 1)
   display_name        = var.dns_label
-  dns_label           = var.dns_label
+ // dns_label           = var.dns_label
   route_table_id      = oci_core_route_table.PublicRT.id
   security_list_ids   = [oci_core_security_list.securitylist.id]
 }

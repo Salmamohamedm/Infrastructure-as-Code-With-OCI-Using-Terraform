5b5e16bc1ab334ff29d3b9f9c13177ffbbbfc091
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-06 16:17:20 +0200

    Update main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -9,28 +9,3 @@ resource "oci_core_subnet" "subnet" {
   security_list_ids   = [oci_core_security_list.securitylist.id]
 }
 
-resource "oci_core_security_list" "securitylist" {
-  display_name   = "SL_public"
-  compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_virtual_network.vcn.id
-
-  egress_security_rules {
-    protocol    = "all"
-    destination = "0.0.0.0/0"
-  }
-
-  ingress_security_rules {
-    protocol = "6"
-    source   = "0.0.0.0/8"
-
-    tcp_options {
-      min = 80
-      max = 80
-    }
-
-    tcp_options {
-      min = 22
-      max = 22
-    }
-  }
-}

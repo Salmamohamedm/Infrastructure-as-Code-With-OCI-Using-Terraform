32a4f60c224103ba2927851f2b293bf78d54bc5b
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-06 16:01:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ module "vcn" {
   // create_internet_gateway = var.create_internet_gateway
  // create_nat_gateway      = var.create_nat_gateway
  // create_service_gateway  = var.create_service_gateway
-  tags                     = var.freeform_tags
+  // tags                     = var.freeform_tags
   vcn_cidrs                = var.vcn_cidrs
   vcn_dns_label            = var.vcn_dns_label
   vcn_name                 = var.vcn_name

fb199c96108ab181ad454c92f4d313514c26b421
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-06 14:53:47 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,15 +16,15 @@ module "vcn" {
 //  label_prefix   = var.label_prefix
 
   # vcn parameters
-  create_internet_gateway = var.create_internet_gateway
+  // create_internet_gateway = var.create_internet_gateway
  // create_nat_gateway      = var.create_nat_gateway
  // create_service_gateway  = var.create_service_gateway
   tags                     = var.freeform_tags
   vcn_cidrs                = var.vcn_cidrs
   vcn_dns_label            = var.vcn_dns_label
   vcn_name                 = var.vcn_name
-  lockdown_default_seclist = var.lockdown_default_seclist
-  attached_drg_id          = var.attached_drg_id
+  // lockdown_default_seclist = var.lockdown_default_seclist
+  // attached_drg_id          = var.attached_drg_id
 }
 
 module "subnet" {
@@ -38,7 +38,7 @@ module "subnet" {
   ig_route_id    = var.create_internet_gateway ? oci_core_route_table.ig[0].id : null
  // nat_route_id   = var.create_nat_gateway ? oci_core_route_table.nat[0].id : null
 
-  freeform_tags = var.freeform_tags
+  // freeform_tags = var.freeform_tags
 
   count = length(var.subnets) > 0 ? 1 : 0
 

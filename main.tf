2a50901751dfb4215742eb74278d762cf9e64595
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-06 15:54:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ module "subnet" {
   vcn_id              =  module.vcn.vcn_id
   vcn_cidr            = var.vcn_cidr
   dns_label           = var.dns_label
-  route_table_id      = oci_core_route_table.PublicRT.id
+  route_table_id      = module.vcn.vcn_id
   security_list_ids   = [oci_core_security_list.securitylist.id]
 }
 

edf89bd6a09de499fc3ceb55ead3ff91d2cfb7c1
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-06 02:45:47 +0200

    Update variables.tf

--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -1,1 +1,7 @@
-
+variable "availability_domain" {}
+variable "compartment_id" {}
+variable "vcn_id" {}
+variable "vcn_cidr" {}
+variable "dns_label" {}
+variable "route_table_id" {}
+variable "security_list_ids" {}

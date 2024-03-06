965e349bb4c3249c28d17972f4f54815ebabc8ee
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-06 19:21:42 +0200

    Update variables.tf

--- modules/RT/variables.tf
+++ modules/RT/variables.tf
@@ -1,9 +1,9 @@
 variable "compartment_id" {}
 variable "vcn_id" {}
 variable "display_name" {}
-variable "route_rules" {
+
  // type = list(object({
   //  destination       = string
-  //  network_entity_id = string
-  // }))
-}
+   variable "network_entity_id" {}
+  
+

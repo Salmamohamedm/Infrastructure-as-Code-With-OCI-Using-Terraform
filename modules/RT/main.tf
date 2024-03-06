0983c79ecc0081bf6e0473364a26b91d816bbef5
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-06 19:19:31 +0200

    Update main.tf

--- modules/RT/main.tf
+++ modules/RT/main.tf
@@ -5,6 +5,6 @@ resource "oci_core_route_table" "PublicRT" {
 
   route_rules {
     destination         = "0.0.0.0/0"
-    network_entity_id   = var. network_entity_id 
+    network_entity_id   = var.network_entity_id 
   }
 }

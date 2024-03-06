6b3ba00684bb4573211f0aa7f2f2c26354fa491b
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-06 03:05:36 +0200

    Update variables.tf

--- modules/Compute/variables.tf
+++ modules/Compute/variables.tf
@@ -1,6 +1,6 @@
 variable "compartment_id" {}
+variable "shape" {}
+variable "display_name" {}
+variable "availability_domain" {}
 variable "subnet_id" {}
 variable "ssh_public_key" {}
-variable "shape" {}
-variable "image_id" {}
-variable "display_name" {}

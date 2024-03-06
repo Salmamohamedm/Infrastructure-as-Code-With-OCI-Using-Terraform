2a29b9530c716260077d45ac0bc9b9dbc9000774
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-03-06 02:33:49 +0200

    Update output.tf

--- modules/VCN/output.tf
+++ modules/VCN/output.tf
@@ -1,7 +1,9 @@
 output "vcn_id" {
-  value = oci_core_vcn.vcn.id
+  description = "The OCID of the VCN."
+  value       = oci_core_virtual_network.vcn.id
 }
 
 output "subnet_ids" {
   value = oci_core_subnet.subnets[*].id
 }
+
